628b9842de44606cbbd47ec174f879d14b4da3f7
k.nosov <k.nosov@rabota.ru>
2021-02-17 18:05:53 +0300

    first commit9

--- src/main/java/settings/DriverSet.java
+++ src/main/java/settings/DriverSet.java
@@ -1,7 +1,6 @@
 package settings;
 
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class DriverSet {
